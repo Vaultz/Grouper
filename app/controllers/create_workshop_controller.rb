c82fcad02a4c078d7dd167ca23c41ba04a86f4a4
Jordan Lefevre <jordanlefevre@exemple.com>
2016-09-30 15:04:22 +0200

    wicked bug

--- app/controllers/create_workshop_controller.rb
+++ app/controllers/create_workshop_controller.rb
@@ -5,16 +5,26 @@ class CreateWorkshopController < ApplicationController
 
   def show
 
-=begin    @workshop = Workshop.new(workshop_params)
+    @workshop = Workshop.new()
     @workshop.user_id = current_user.id # Give the current user id at the new workshop
-=end
+    @workshop.name = "TestinController"
     case step
     when :create
       @workshop = Workshop.new
     when :validate
-
     end
+    
     render_wizard
+  end
+
+
+  def update
+    @workshop = Workshop.new()
+    @workshop.name = "TestinController"
+  end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def workshop_params
+    params.require(:workshop).permit(:name, :description, :user_id, :teacher, :begins, :ends, :teamgeneration, :teamnumber)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ Rails.application.routes.draw do
   root :to => "home#index"
 
   resources :create_workshop
+  match "create_workshop/validate", to: "create_workshop#validate", via: "put"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
