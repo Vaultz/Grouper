05c7ba9c5fe398b1751167c064b43f35fba3163a
Jordan Lefevre <jordanlefevre@exemple.com>
2016-09-30 16:32:55 +0200

    Wicked works ... well almost

--- app/controllers/create_workshop_controller.rb
+++ app/controllers/create_workshop_controller.rb
@@ -5,22 +5,48 @@ class CreateWorkshopController < ApplicationController
 
   def show
 
-    @workshop = Workshop.new()
-    @workshop.user_id = current_user.id # Give the current user id at the new workshop
-    @workshop.name = "TestinController"
+
+    #@workshop.user_id = current_user.id # Give the current user id at the new workshop
+
     case step
     when :create
       @workshop = Workshop.new
+      session[:workshop] = nil
     when :validate
+
+=begin
+      @workshop = Workshop.last
+      @params = params
+=end
+      @workshop = Workshop.new(session[:workshop])
     end
-    
+
     render_wizard
   end
 
 
   def update
-    @workshop = Workshop.new()
-    @workshop.name = "TestinController"
+
+=begin
+    @workshop = Workshop.new(workshop_params)
+    @workshop.user_id = current_user.id # Give the current user id at the new workshop
+    render_wizard @workshop
+=end
+    case step
+    when :create
+      @workshop = Workshop.new(workshop_params)
+      @workshop.user_id = current_user.id # Give the current user id at the new workshop
+      session[:workshop] = @workshop.attributes
+      redirect_to next_wizard_path
+    when :validate
+      @workshop = Workshop.new(session[:workshop])
+      @workshop.save
+      #redirect_to workshop_path(@workshop)
+    end
+  end
+
+  def finish_wizard_path
+   workshops_path()
   end
   # Never trust parameters from the scary internet, only allow the white list through.
   def workshop_params
