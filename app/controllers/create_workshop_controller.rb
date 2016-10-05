286f6467c65cfb1a34ed44fc2b04234ab784574f
sebberrou <seb.berrou@laposte.net>
2016-10-05 16:50:49 -0400

    Wicked finally working!

--- app/controllers/create_workshop_controller.rb
+++ app/controllers/create_workshop_controller.rb
@@ -10,48 +10,41 @@ class CreateWorkshopController < ApplicationController
     when :create
       # @workshop will be use to make the corresponding form in the view
       @workshop = Workshop.new
-      
       #initialize the session variable that will be use to store the datas before saving to the database
       session[:workshop] = nil
     when :validate
-
-      #@workshop = Workshop.new(session[:workshop])
-    #  @project = Project.new
+      @workshop = Workshop.new(session[:workshop])
     end
+
     # render the view corresponding to the actual step
     render_wizard
   end
-
 # each step of the wizard will go through the update action before showing the next step
   def update
-
-=begin
-    @workshop = Workshop.new(workshop_params)
-    @workshop.user_id = current_user.id # Give the current user id at the new workshop
-    render_wizard @workshop
-=end
+    #@workshop = Workshop.new(workshop_params)
+    #@workshop.user_id = current_user.id # Give the current user id at the new workshop
+    #render_wizard @workshop
     case step
     when :create
       # we create a new variable session with the nexly acquired info on the workshop
       #then we redirect to the next step defore wicked can save to the database
-    #  @workshop = Workshop.new(workshop_params)
-    #  @workshop.user_id = current_user.id # Give the current user id at the new workshop
-    #  session[:workshop] = @workshop.attributes
-    #  redirect_to next_wizard_path
-    when :validate
-=begin        @workshop = Workshop.new(session[:workshop])
-    if @workshop.save
-        redirect_to workshops_path(), notice: "Thank you for signing up."
+    @workshop = Workshop.new(workshop_params)
+    @workshop.user_id = current_user.id # Give the current user id at the new workshop
+    session[:workshop] = @workshop.attributes
+    redirect_to next_wizard_path
+    #When validating the last form the step won't be 'validate' but something else, so we put else 
+    else
+      @workshop = Workshop.new(session[:workshop])
+      if @workshop.save
+          redirect_to workshops_path(), notice: "Workshop was successfully created"
       end
-      #redirect_to_finish_wizard
     end
-=end
   end
-=begin
+  private
+
   def finish_wizard_path
    workshops_path()
   end
-=end
   # Never trust parameters from the scary internet, only allow the white list through.
   def workshop_params
     params.require(:workshop).permit(:name, :description, :user_id, :teacher, :begins, :ends, :teamgeneration, :teamnumber)
