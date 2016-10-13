3e3f9b4ec48b56c27e3066f6096d1681d1b2e0fd
Vaultz <paulvialart@hotmail.fr>
2016-10-13 19:09:37 +0200

    Fixing a bug

--- app/controllers/create_workshop_controller.rb
+++ app/controllers/create_workshop_controller.rb
@@ -12,6 +12,9 @@ class CreateWorkshopController < ApplicationController
       @workshop = Workshop.new
       #initialize the session variable that will be use to store the datas before saving to the database
       session[:workshop] = nil
+
+      session[:users] = User.all
+
     when :validate
       @workshop = Workshop.new(session[:workshop])
     end
