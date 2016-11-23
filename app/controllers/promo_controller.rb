cf306afd06023ebf6a686f2d793b596aea3b9863
Sébastien B <seb.berrou@laposte.net>
2016-11-23 18:06:04 +0100

    checking before promo view

--- app/controllers/promo_controller.rb
+++ app/controllers/promo_controller.rb
@@ -1,5 +1,9 @@
 class PromoController < ApplicationController
   def index
+
     @user_promo = User.where('year = ?', @promo)
+    unless @user_promo.any?
+      render file: "#{Rails.root}/public/404.html" , status: :not_found
+    end
   end
 end
